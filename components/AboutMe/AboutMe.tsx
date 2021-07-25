2604623e435d93f46ac50431c84d35ab710455f6
Jan Sopko <jan.sopko@ringieraxelspringer.sk>
2021-07-25 13:21:06 +0200

    img to next/image, favicon

--- components/AboutMe/AboutMe.tsx
+++ components/AboutMe/AboutMe.tsx
@@ -14,11 +14,16 @@ export const AboutMe = () => {
                 <h1>
                     {mainHeadingSection[language].text}
                 </h1>
-                <h2>{language}</h2>
                 <section className="about-me-content">
                 {/*<Image src={myPicture} alt="Photo of the author" height={200} width={150} id="photo"/>*/}
                     <figure id="photo">
                         {/*<img src="https://gitlab.i.etech.sk/uploads/-/system/user/avatar/230/avatar.png?width=90" alt="Photo of the author"/>*/}
+                        <Image
+                            src={myPicture}
+                            width={108}
+                            height={192}
+                            alt="Photo of the author"
+                        />
                     </figure>
                     {/*<div id="photo" />*/}
                     blablabla text o mne blablabla text o mne blablabla text o mne blablabla text o mne blablabla text o mne
--- components/LanguagesSelect/LanguagesSelect.tsx
+++ components/LanguagesSelect/LanguagesSelect.tsx
@@ -12,12 +12,20 @@ import {LanguagesEnum} from "../../language-config/languages.enum";
 import IconButton from '@material-ui/core/IconButton';
 import Image from "next/image";
 
+const imageLoader = (props: {src: string}) => {
+    return props.src;
+}
+
 export const LanguagesSelect = (props: {
     currentLanguage: LanguagesEnum,
     setLanguage: (...args: any[]) => any | void
 }) => {
     const LANGUAGE_LABEL = 'language-label';
     const {currentLanguage} = props;
+
+    const flagLoader = (props: {src: string}) => {
+        return props.src;
+    }
 
     return(
             <FormControl>
@@ -35,15 +43,24 @@ export const LanguagesSelect = (props: {
                 onChange={e => props.setLanguage(e.target.value)}
             >
                 {
-                    languages.map(lang => (
+                    languages.map(lang => {
+
+                        return (
                         <MenuItem value={lang.short} key={lang.short}>
                             <IconButton>
                                 {/*<img src={lang.flagUrl} alt={`${lang.short}-flag`} height={20} width={20}/>*/}
-
+                                <Image
+                                    loader={flagLoader}
+                                    src={lang.flagUrl}
+                                    width={20}
+                                    height={20}
+                                    alt={`${lang.short}-flag`}
+                                />
                             </IconButton>
                             {lang.full}
                         </MenuItem>
-                    ))
+                        )
+                    })
                 }
             </Select>
             </FormControl>
