d8d1a435e9f2aabe989a65935db9cfb83cebd7a0
Jan Sopko <jan.sopko@ringieraxelspringer.sk>
2021-07-26 08:32:23 +0200

    default language slovak

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import type {AppProps} from 'next/app'
 import {createContext, useState} from "react";
 import {LanguagesEnum} from "../language-config/languages.enum";
 
-export const LanguageContext = createContext<LanguagesEnum>(LanguagesEnum.ENGLISH);
+
+const DEFAULT_LANGUAGE: LanguagesEnum = LanguagesEnum.SLOVAK;
+export const LanguageContext = createContext<LanguagesEnum>(DEFAULT_LANGUAGE);
 
 function MyApp({ Component, pageProps }: AppProps) {
-  const [language, setLanguage] = useState(LanguagesEnum.ENGLISH); //todo prerobit
+  const [language, setLanguage] = useState(DEFAULT_LANGUAGE);
 
   return (
       <LanguageContext.Provider value={language}>
